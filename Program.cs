7189a3f3d8b332898d9f3dff100298229017eccc
Sjors van Gelderen <sjors.van.gelderen@dji.minjus.nl>
2022-10-17 22:38:59 +0200

    Base code commit

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace wavfix
+{
+  class Program
+  {
+    private static void Main(string[] args)
+    {
+      for(var i = 0; i < args.Length; i++)
+      {
+        Console.WriteLine("Processing file {0} out of {1}...", i + 1, args.Length);
+
+        var path = args[i];
+        var fileName = Path.GetFileName(path);
+        var outPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
+
+        if(File.Exists(path))
+        {
+          Console.WriteLine("Inspecting {0}...", fileName);
+
+          var format = new byte[2];
+          var expectedFormat = new byte[] { 0x01, 0x00 };
+
+          using(var fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+          {
+            fs.Position = 20;
+            fs.Read(format, 0, expectedFormat.Length);
+
+            fs.Close();
+            fs.Dispose();
+          }
+
+          if(format[0].Equals(expectedFormat[0]) && format[1].Equals(expectedFormat[1]))
+          {
+            Console.WriteLine("This file is fine.");
+
+            continue;
+          }
+          else 
+          {
+            Console.WriteLine("This file needs fixing! Getting to work...");
+          }
+
+          Console.WriteLine("Copying the file...");
+
+          File.Copy(path, outPath, true);
+
+          using(var fs = new FileStream(outPath, FileMode.Open, FileAccess.Write))
+          {
+            fs.Position = 20;
+            fs.Write(expectedFormat, 0, expectedFormat.Length);
+          }
+
+          Console.WriteLine("Fixed this file, moving to the next one...");
+        }
+      }
+
+      Console.WriteLine("All done!");
+      Console.ReadKey();
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # wavfix
 Fixing utility for E-8305: Unsupported File Format with WAV files on Pioneer CDJ
+
+## Usage
+1. Select all WAV files you want to fix in Explorer
+2. Drag and drop them onto the wavfix.exe
+3. Find your fixed files in the same folder where wavfix.exe is installed
